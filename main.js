b62cfd7ca7d45130e872730fe7614f99d9c71ff9
unknown <tombirbeck47@gmail.com>
2023-03-08 22:58:44 +0000

    added event listeners for bill and people amount

--- main.js
+++ main.js
@@ -10,6 +10,32 @@ const fifty = document.querySelector('.fifty')
 const custom = document.querySelector('.custom')
 const reset = document.querySelector('.reset-button')
 
+// console.log(five.value)
+
+let billAmount = 0;
+let tippingPercent = 0;
+let amountOfPeople = 0;
+
+const handleBill = (e) => {
+    billAmount = e.target.value
+    console.log(billAmount)
+    calculateAmountToPay(billAmount, tippingPercent, amountOfPeople)
+}
+
+const handleTip = (button) => {
+    tippingPercent = Number(button.value)
+}
+
+const handlePeople = (e) => {
+    amountOfPeople = e.target.value
+    console.log(amountOfPeople)
+    calculateAmountToPay(billAmount, tippingPercent, amountOfPeople)
+}
+
+bill.addEventListener('change', handleBill)
+people.addEventListener('change', handlePeople)
+five.addEventListener('click', () => {handleTip(five)})
+
 const calculateTip = (total, tipPercentage) => {
     return ((total/100) * tipPercentage)
 }
@@ -22,9 +48,8 @@ const calculateSplitBill = (total, people) => {
     return total / people
 }
 
-const amountToPay = (bill, tipPercentage, people) => {
+const calculateAmountToPay = (bill, tipPercentage, people) => {
 const total = bill + calculateTip(bill, tipPercentage)
+console.log("hey")
 return total / people
 }
-
-console.log(amountToPay(100,10,2))
