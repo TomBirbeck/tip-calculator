a3b8346828109e267a610e0b07bea63670498a89
unknown <tombirbeck47@gmail.com>
2023-03-08 20:28:08 +0000

    created functions to calculate tip, tip per person, the split of the bill and the amount to pay per person

--- main.js
+++ main.js
@@ -7,4 +7,24 @@ const ten = document.querySelector('.ten')
 const fifteen = document.querySelector('.fifteen')
 const twentyFive = document.querySelector('.twenty-five')
 const fifty = document.querySelector('.fifty')
+const custom = document.querySelector('.custom')
 const reset = document.querySelector('.reset-button')
+
+const calculateTip = (total, tipPercentage) => {
+    return ((total/100) * tipPercentage)
+}
+
+const calculateTipPerPerson = (total, tipPercentage, people) => {
+    return calculateTip(total, tipPercentage) / people
+}
+
+const calculateSplitBill = (total, people) => {
+    return total / people
+}
+
+const amountToPay = (bill, tipPercentage, people) => {
+const total = bill + calculateTip(bill, tipPercentage)
+return total / people
+}
+
+console.log(amountToPay(100,10,2))
